210229992bd0dc2717f70af39f04afb4ee5cd36f
Svleh <mahmoud.saleh.ps@gmail.com>
2023-09-20 09:39:33 -0700

    done

---
+++ 5-string_toupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+char *string_toupper(char *n)
+{
+	int i;
+
+	i = 0;
+	while (n[i] != '\0')
+	{
+		if (n[i] >= 'a' && n[i] <= 'z')
+			n[i] = n[i] - 32;
+		i++;
+	}
+	return (n);
+}
+
